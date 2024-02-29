afd99a927cc272a5f8480f300279d6823ad31345
Ryan Ferris <r.ferris137@gmail.com>
2024-02-29 13:02:08 +0000

    tutor fix

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,29 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
-from .models import Event
+from .models import Post
+
+# Create your views here.
 
 
-class EventsList(generic.ListView):
+class PostList(generic.ListView):
+    queryset = Post.objects.filter(status=1)
+    template_name = "blog/index.html"
+    paginate_by = 6
 
 
-    model = Event
-    template_name = "index.html"
-    paginate_by = 12
+def post_detail(request, slug):
+    """
+    Display an individual :model:`blog.Post`.
 
+    **Context**
 
+    ``post``
+        An instance of :model:`blog.Post`.
 
+    **Template:**
 
-def event_detail(request, event_id):
-    
-    queryset = Event.objects.all()
-    event = get_object_or_404(queryset, event_id=event_id)
-
-    return render(
-        request,
-        "events/event_detail.html",
-        {"event": event}
-    )
+    :template:`blog/post_detail.html`
+    """
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+    return render(request, "blog/post_detail.html", {"post": post},)
