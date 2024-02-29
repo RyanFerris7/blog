ae285b303986ca8e28e0da6b875eddc7dfad9645
Ryan Ferris <r.ferris137@gmail.com>
2024-02-29 00:43:05 +0000

    add content to index!

--- blog/views.py
+++ blog/views.py
@@ -1,33 +1,25 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
-from .models import Post
+from .models import Event
 
 
-# Create your views here.
-class PostList(generic.ListView):
-    queryset = Post.objects.filter(status=1)
-    template_name = "blog/index.html"
-    paginate_by = 6
+class EventsList(generic.ListView):
 
-def post_detail(request, slug):
-    """
-    Display an individual :model:`blog.Post`.
 
-    **Context**
+    model = Event
+    template_name = "index.html"
+    paginate_by = 12
 
-    ``post``
-        An instance of :model:`blog.Post`.
 
-    **Template:**
 
-    :template:`blog/post_detail.html`
-    """
 
-    queryset = Post.objects.filter(status=1)
-    post = get_object_or_404(queryset, slug=slug)
+def event_detail(request, event_id):
+    
+    queryset = Event.objects.all()
+    event = get_object_or_404(queryset, event_id=event_id)
 
     return render(
         request,
-        "blog/post_detail.html",
-        {"post": post},
+        "events/event_detail.html",
+        {"event": event}
     )
